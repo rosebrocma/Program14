4ef0aa158c38a7220daac1b492c58d3fe71f1402
rosebrocma <rosebrocma@mnstate.edu>
2016-04-22 12:45:23 -0500

    Update modCustomSet.py

--- modCustomSet.py
+++ modCustomSet.py
@@ -1,5 +1,21 @@
 class CustomSet:
-def __init__
+def __init__(self, setList):
+        ''''''
+        aList = []
+        for num in setList:
+            if num not in aList:
+                aList.append(num)
+        self._SetElements = aList
+
+    def __str__(self):
+        ''''''
+        tmp = ''
+        
+        tmp += '{'
+        for num in self._SetList:
+            tmp += str(num) + ','
+        tmp += '}'
+        return tmp
 
 
 
