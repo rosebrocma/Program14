3feeac60152f2108d9ac0fc1f1d18a757e0e86a2
Spencer Sivertson <sivertsosp@mnstate.edu>
2016-04-26 18:33:03 -0500

    Added__le__ to modCustomSet.py and test for it in p14Test.py

--- P14Test.py
+++ P14Test.py
@@ -1,1 +1,16 @@
 from modCustomSet import *
+#testing str method for both set1 and set2
+print("Creating and printing set1", end= " ");set1 =CustomSet([1,1,2,2,3,3])
+print(set1)
+print("Creating and printing set2", end= " ");set2 = CustomSet([1,2,3,4,4])
+print(set2)
+#Testing <= or subset
+print("Testing subset if set1 is a subset of set2", end= " ")
+subset =(set1 <= set2)
+print(subset)
+print("Testing if set2 is a subset of set1", end= " ")
+if set2 <= set1:print("Yes")
+else:
+    print("not a subset")
+
+
--- modCustomSet.py
+++ modCustomSet.py
@@ -1,11 +1,11 @@
 class CustomSet:
-def __init__(self, setList):
+    def __init__(self, setList):
         ''''''
         aList = []
         for num in setList:
             if num not in aList:
                 aList.append(num)
-        self._SetElements = aList
+        self._SetList = aList
 
     def __str__(self):
         ''''''
@@ -16,8 +16,26 @@ def __init__(self, setList):
             tmp += str(num) + ','
         tmp += '}'
         return tmp
-
-
-
-
-
+    
+    def __le__(self,other):
+        """
+        Descriptions: Makes it able to so we can see objects of type CustomSet
+        are a subset one another.
+        PreConditions: None
+        PostConditions: None
+        """
+        ct = 0
+        numsInOther=0
+        while ct < len(self._SetList):
+            for self._SetList[ct] in self._SetList:
+                if self._SetList[ct] in other._SetList:
+                    numsInOther+=1
+                    ct+=1
+                elif self._SetList[ct] not in other._SetList:
+                    numsInOther+=0
+                    ct+=1
+            if numsInOther == len(self._SetList):
+                return  True
+            else:
+                return False
+   
