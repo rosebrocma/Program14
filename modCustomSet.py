5d068482ab88129c201837da10c5cd6c484dc73c
Spendy33 <Spendy33@users.noreply.github.com>
2016-04-27 14:01:13 -0500

    Update modCustomSet.py

--- modCustomSet.py
+++ modCustomSet.py
@@ -5,14 +5,14 @@ class CustomSet:
         for num in setList:
             if num not in aList:
                 aList.append(num)
-        self._SetList = aList
+        self._SetElements = aList
 
     def __str__(self):
         ''''''
         tmp = ''
         
         tmp += '{'
-        for num in self._SetList:
+        for num in self._SetElements:
             tmp += str(num) + ','
         tmp += '}'
         return tmp
@@ -21,20 +21,20 @@ class CustomSet:
         """
         Descriptions: Makes it able to so we can see objects of type CustomSet
         are a subset one another.
-        PreConditions: None
+        PreConditions: Both objects must be of type CustomSet.
         PostConditions: None
         """
         ct = 0
         numsInOther=0
-        while ct < len(self._SetList):
-            for self._SetList[ct] in self._SetList:
-                if self._SetList[ct] in other._SetList:
+        while ct < len(self._SetElements):
+            for self._SetElements[ct] in self._SetElements:
+                if self._SetElements[ct] in other._SetElements:
                     numsInOther+=1
                     ct+=1
-                elif self._SetList[ct] not in other._SetList:
+                elif self._SetElements[ct] not in other._SetElements:
                     numsInOther+=0
                     ct+=1
-            if numsInOther == len(self._SetList):
+            if numsInOther == len(self._SetElements):
                 return  True
             else:
                 return False
@@ -43,20 +43,20 @@ class CustomSet:
         """
         Descriptions: Makes it able to so we can see objects of type CustomSet
         are a superset of one another.
-        PreConditions: None
+        PreConditions: Both objects must be of type CustomSet.
         PostConditions: None
         """
         ct = 0
         numsInOther=0
-        while ct < len(other._SetList):
-            for other._SetList[ct] in other._SetList:
-                if other._SetList[ct] in self._SetList:
+        while ct < len(other._SetElements):
+            for other._SetElements[ct] in other._SetElements:
+                if other._SetElements[ct] in self._SetElements:
                     numsInOther+=1
                     ct+=1
-                elif other._SetList[ct] not in self._SetList:
+                elif other._SetElements[ct] not in self._SetElements:
                     numsInOther+=0
                     ct+=1
-            if numsInOther == len(other._SetList):
+            if numsInOther == len(other._SetElements):
                 return  True
             else:
                 return False
@@ -67,4 +67,4 @@ class CustomSet:
         PreConditions: None
         PostConditions: None
         """
-        return len(self._SetList)
+        return len(self._SetElements)
