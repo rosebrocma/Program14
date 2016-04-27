19bbbd47181149f6b0edf6e998f3036492c2c639
rosebrocma <rosebrocma@mnstate.edu>
2016-04-27 12:37:59 -0500

    removed excessive plus symbols

--- P14Test.py
+++ P14Test.py
@@ -1,16 +1,16 @@
 from modCustomSet import *
 c1 = CustomSet([1, 3, 5, 1])
-+
-+c2 = CustomSet([1, 2, 3, 4])
-+c3 = CustomSet([])
-+c4 = CustomSet([])
-+print(1 in c1)
-+print(6 in c2)
-+
-+c3 = c1 - c2
-+
-+c4 = c2 - c1
-+
-+print(c3)
-+print()
-+print(c4)
+
+c2 = CustomSet([1, 2, 3, 4])
+c3 = CustomSet([])
+c4 = CustomSet([])
+print(1 in c1)
+print(6 in c2)
+
+c3 = c1 - c2
+
+c4 = c2 - c1
+
+print(c3)
+print()
+print(c4)
