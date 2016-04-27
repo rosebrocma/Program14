12012b5dbf00212724bf0e2ac8938496061c2e6d
Spendy33 <Spendy33@users.noreply.github.com>
2016-04-27 14:09:58 -0500

    Update modCustomSet.py

--- modCustomSet.py
+++ modCustomSet.py
@@ -27,46 +27,7 @@ class CustomSet:
                 tmp += '}'
                 return tmp
         
-    def __add__(self, other):
-        '''
-        Description: (Matthew R. wrote this) adds to CustomSet objects together and returns it
-        PreConditions: self and other must both be CustomSet objects
-        PostConditions: creates a new CustomSet object
-        '''
-        aList = self._SetElements + other._SetElements
-        return CustomSet(aList)   
-        
-    def __and__(self, other):
-        '''Descriptioin - (Ryan wrote this method)takes the two sets and returns a new set with the union of the two
-            PreCondition - self and other must both be of CustomSet objects
-            PostCondition - a new CustomSet object is created'''
-        newSet = []
-        aList = self._SetElements
-        for ele in aList:
-            if ele in other._SetElements:
-                newSet.append(ele)
-            return CustomSet(newSet)
-
-
-    def __sub__(self, other):
-        '''Description - (Ryan wrote this method) takes the difference between two sets and returns the result
-        PreConditon - self and other must both be of the type CustomSet
-        PostCondition - a new CustomSet is created'''
-        aList = self._SetElements
-        newSet = []
-        for ele in aList:
-            if ele not in other._SetElements:
-                newSet.append(ele)
-            return CustomSet(newSet)
-        
-        
-    def __contains__(self, num):
-        '''Description - (Ryan wrote this method) checks to see if a given number is in the CustomSet
-        PreCondition - self must be of type CustomSet
-        PostCondition - a correct statement is given according to the truth of the statment'''
-        return num in self._SetElements
-
-
+  
     def __le__(self,other):
         """
         Descriptions: Spencer(Spendy33)made this. Makes it able to so we can see objects of type CustomSet
