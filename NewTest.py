9c72863581e304aaf61fc75bba3d6101a5d55def
RyanAndersonMSUM <andersory@mnstate.edu>
2016-04-27 12:45:36 -0500

    Create NewTest.py

---
+++ NewTest.py
@@ -0,0 +1,20 @@
+from myMethods import *
+c1 = CustomSet([1, 3, 5, 1])
+
+c2 = CustomSet([1, 2, 3, 4])
+c3 = CustomSet([])
+c4 = CustomSet([])
+print("Testing In", 1 in c1)
+print("Testing In", 6 in c2)
+
+c3 = c1 - c2
+
+c4 = c2 - c1
+
+print("Testing Difference:",c3)
+print()
+print("Testing Difference:",c4)
+
+print()
+c3 = c1  & c2
+print("Testing Union", c3)
