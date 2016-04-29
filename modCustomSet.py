911fe8a71136a9959a82be046874d0bb2cf4ad09
rosebrocma <rosebrocma@mnstate.edu>
2016-04-29 10:26:49 -0500

    commited speedy33's methods

--- modCustomSet.py
+++ modCustomSet.py
@@ -65,6 +65,58 @@ class CustomSet:
                 PreCondition - self must be of type CustomSet
                 PostCondition - a correct statement is given according to the truth of the statment'''
                 return num in self._SetElements
+        
+        def __le__(self,other):
+                """
+                Descriptions: Spencer(Spendy33)made this. Makes it able to so we can see objects of type CustomSet
+                are a subset one another.
+                PreConditions: Objects must be of type CustomSet
+                PostConditions: None
+                """
+                ct = 0
+                numsInOther=0
+                while ct < len(self._SetElements):
+                    for self._SetElements[ct] in self._SetElements:
+                        if self._SetElements[ct] in other._SetElements:
+                            numsInOther+=1
+                            ct+=1
+                        elif self._SetElements[ct] not in other._SetElements:
+                            numsInOther+=0
+                            ct+=1
+                    if numsInOther == len(self._SetElements):
+                        return  True
+                    else:
+                        return False
+
+        def __ge__(self,other):
+                """
+                Descriptions:Spencer(Spendy33)made this. Makes it able to so we can see objects of type CustomSet
+                are a superset of one another.
+                PreConditions: Objects must be of type CustomSet
+                PostConditions: None
+                """
+                ct = 0
+                numsInOther=0
+                while ct < len(other._SetElements):
+                    for other._SetElements[ct] in other._SetElements:
+                        if other._SetElements[ct] in self._SetElements:
+                            numsInOther+=1
+                            ct+=1
+                        elif other._SetElements[ct] not in self._SetElements:
+                            numsInOther+=0
+                            ct+=1
+                    if numsInOther == len(other._SetElements):
+                        return  True
+                    else:
+                        return False
+
+        def __len__(self):
+                """
+                Description:Spencer(Spendy33)made this.Makes it able to use len on objects of type CustomSet.
+                PreConditions: None
+                PostConditions: None
+                """
+                return len(self._SetElements)
 
 
     
